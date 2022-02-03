d744b7ed9f033c84b1452ae16f6b8a2635f0f80a
Andrea Alicino <andrea.alicino@gmail.com>
2022-02-03 21:43:45 +0100

    test with lambda expression

---
+++ Assets/Scripts/DelegateActionEvents_tut/MainLambda.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainLambda : MonoBehaviour
+{
+    public Action<int, int> Sum;
+
+    private void Start()
+    {
+        Sum = (a, b) => Debug.Log(a + b);
+
+        Sum(5, 12);
+    }
+
+    void CalculateSum(int a, int b) 
+    {
+        var total = a + b;
+        Debug.Log("Total: " + total);
+    }
+}
